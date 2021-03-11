b1ed28e00a0b90e7acd3a51c02fa5482ecdc8f9d
Adam Khalil <adam.khalil@edu.fh-joanneum.at>
2021-03-11 14:04:47 +0100

    New Class AdamsDrink with first implementation

---
+++ src/main/java/at/fhj/iit/AdamsDrink.java
@@ -0,0 +1,17 @@
+/**
+* Author: Adam Khalil
+* Class that extends SimpleDrink
+* Date: 11.03.2021
+*/
+
+package at.fhj.iit;
+/**
+ * AdamsDrinkis an extension of a simple drink and tastes better
+ *
+ */
+public class AdamsDrink extends SimpleDrink {
+
+    AdamsDrink(String name, Liquid l) {
+        super(name, l);
+    }
+}k
