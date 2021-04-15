7855356a4e924ba8990830a8c8e7d59a92d7fe5c
Fabian Schedler <dev.fabian.schedler@gmail.com>
2021-04-15 10:59:33 +0200

    generated FabiDrink Test

---
+++ src/test/java/at/fhj/iit/FabiDrinkTest.java
@@ -0,0 +1,33 @@
+package at.fhj.iit;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FabiDrinkTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void addLiquid() {
+    }
+
+    @Test
+    void getVolume() {
+    }
+
+    @Test
+    void getAlcoholPercent() {
+    }
+
+    @Test
+    void isAlcoholic() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
