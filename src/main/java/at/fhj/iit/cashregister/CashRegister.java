e8477944cb910ab2a46f1155749009c39d9275e6
Adam Khalil <adam.khalil@edu.fh-joanneum.at>
2021-06-03 09:11:40 +0200

    Implementaion of the getHighAlcoholicRevenue method

--- src/main/java/at/fhj/iit/cashregister/CashRegister.java
+++ src/main/java/at/fhj/iit/cashregister/CashRegister.java
@@ -8,45 +8,56 @@ import java.util.List;
 public class CashRegister {
     private List<Transaction> transactions = new ArrayList<>();
 
-    void sell(Buyable product, String seller, Date sellDate){
+    void sell(Buyable product, String seller, Date sellDate) {
         transactions.add(new Transaction(product, seller, sellDate));
     }
 
-    double getNoneAlcoholicRevenue(){
+    double getNoneAlcoholicRevenue() {
         // TODO implement Fabi
         return 0;
     }
 
     /**
      * Total sales of low alcoholic drinks
-     * @return lowAlcoholRevenue made with alholic drinks that are 16% or less
+     *
+     * @return lowAlcoholRevenue made with alcoholic drinks that are 16% or less
      */
-    double getLowAlcoholicRevenue(){
+    double getLowAlcoholicRevenue() {
         double lowAlcoholRevenue = 0;
-        for (Transaction t : transactions){
-            if(t.getProduct().isAlcoholic() && t.getProduct().getAlcoholPercentage()<=16) {
+        for (Transaction t : transactions) {
+            if (t.getProduct().isAlcoholic() && t.getProduct().getAlcoholPercentage() <= 16) {
                 lowAlcoholRevenue += t.getProduct().getPrice();
             }
         }
         return lowAlcoholRevenue;
     }
 
-    double getHighAlcoholicRevenue(){
-        // TODO implement Adam
-        return 0;
+    /**
+     * Total sales of low alcoholic drinks
+     *
+     * @return highAlcoholRevenue made with alcoholic drinks that are higher than 16%
+     */
+    double getHighAlcoholicRevenue() {
+        double highAlcoholRevenue = 0;
+        for (Transaction t : transactions) {
+            if (t.getProduct().getAlcoholPercentage() > 16) {
+                highAlcoholRevenue += t.getProduct().getPrice();
+            }
+        }
+        return highAlcoholRevenue;
     }
 
-    double getDayRevenue(Date day){
+    double getDayRevenue(Date day) {
         // TODO implement Alex
         return 0;
     }
 
-    double getPersonRevenue(String name){
+    double getPersonRevenue(String name) {
         // TODO implement Alex
         return 0;
     }
 
-    double getPersonDayRevenue(String name, Date day){
+    double getPersonDayRevenue(String name, Date day) {
         // TODO implement Alex
         return 0;
     }
